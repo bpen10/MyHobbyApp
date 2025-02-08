c0d091c0d78d9cbffc24a33fea3a4b26e9f234a8
Braden Penner <bpen10@byu.edu>
2025-02-07 18:19:12 -0700

    Add project files.

---
+++ MyHobbyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHobbyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
